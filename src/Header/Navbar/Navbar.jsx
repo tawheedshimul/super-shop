bf1812d12a849aa6e37a2202189f59c1951eb756
tawheedshimul <tawheedshimul@gmail.com>
2023-07-20 18:34:28 +0600

    update

--- src/Header/Navbar/Navbar.jsx
+++ src/Header/Navbar/Navbar.jsx
@@ -8,24 +8,19 @@ const ShoppingNavbar = () => {
     <nav className="bg-white p-4 shadow fixed top-0 left-0 right-0 z-40">
       <div className="flex items-center justify-between container mx-auto">
 
-        {/* hemberger menu  */}
         <div className='flex items-center'>
-          <IoIosHome className='text-3xl text-gray-500'></IoIosHome>
-          <div className="mx-2 h-10 border-r border-gray-500"></div>
-        </div>
+          {/* hemberger menu  */}
+          <div className='flex items-center'>
+            <IoIosHome className='text-3xl text-gray-500'></IoIosHome>
+            <div className="mx-2 h-10 border-r border-gray-500"></div>
+          </div>
 
-
-
-        {/* logos section  */}
-        <div className="flex items-center">
-          <a href="/" className="text-black text-xl font-semibold hover:text-blue-300">
-            Super-Shop
-          </a>
-        </div>
-
-        {/* shoping section  */}
-        <div className="hidden md:flex items-center ml-4">
-
+          {/* logos section  */}
+          <div className="mr-4 flex items-center">
+            <a href="/" className="text-black text-xl font-semibold hover:text-blue-300">
+              Super-Shop
+            </a>
+          </div>
         </div>
 
         {/* search section */}
@@ -33,25 +28,27 @@ const ShoppingNavbar = () => {
           <SearchBar></SearchBar>
         </div>
 
-        {/* login section  */}
-        <div className="ml-4">
-          <button className='text-black hover:text-blue-300'>
-            <GiLockedBox className='text-2xl'></GiLockedBox>
-          </button>
-        </div>
+        <div className='flex items-center'>
+          {/* login section  */}
+          <div className="ml-4">
+            <button className='text-black hover:text-blue-300'>
+              <svg xmlns="http://www.w3.org/2000/svg" height="25" viewBox="0 -960 960 960" width="25"><path d="M481-120v-60h299v-600H481v-60h299q24 0 42 18t18 42v600q0 24-18 42t-42 18H481Zm-55-185-43-43 102-102H120v-60h363L381-612l43-43 176 176-174 174Z" /></svg>
+            </button>
+          </div>
 
-        {/* cart section  */}
-        <div className="relative ml-4">
-          <button className="text-black hover:text-blue-300">
-            <GiShoppingCart className="text-2xl" />
-          </button>
-          <span className="bg-red-500 text-white absolute -top-1 -right-1 text-xs w-5 h-5 flex items-center justify-center rounded-full">
-            3
-          </span>
+          {/* cart section  */}
+          <div className="relative ml-4">
+            <button className="text-black hover:text-blue-300">
+              <GiShoppingCart className="text-2xl" />
+            </button>
+            <span className="bg-red-500 text-white absolute -top-1 -right-1 text-xs w-5 h-5 flex items-center justify-center rounded-full">
+              3
+            </span>
+          </div>
         </div>
       </div>
       <div className='mt-2 block sm:hidden'>
-      <SearchBar></SearchBar>
+        <SearchBar></SearchBar>
       </div>
     </nav>
   );
