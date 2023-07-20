75b7ac0601b119e5b92eb17433794884351d07f8
tawheedshimul <tawheedshimul@gmail.com>
2023-07-20 16:24:41 +0600

    update

--- src/Pages/Home/Hero/Hero.jsx
+++ src/Pages/Home/Hero/Hero.jsx
@@ -2,20 +2,20 @@ import React from 'react';
 
 const HeroSection = () => {
   return (
-    <section className="bg-white dark:bg-gray-900">
+    <section className="bg-white">
       <div className="grid max-w-screen-xl px-4 py-8 mx-auto gap-4 md:gap-8 lg:gap-16 md:px-8 lg:py-12 lg:grid-cols-2">
         <div className="flex flex-col justify-center md:pr-8 lg:pr-12">
-          <h1 className="text-3xl font-extrabold tracking-tight leading-tight md:text-4xl lg:text-5xl xl:text-6xl dark:text-white">
+          <h1 className="text-3xl font-extrabold tracking-tight leading-tight md:text-4xl lg:text-5xl xl:text-6xl">
             Payments tool for software companies
           </h1>
-          <p className="mt-4 text-base text-gray-600 md:text-lg lg:text-xl dark:text-gray-400">
+          <p className="mt-4 text-base text-gray-600 md:text-lg lg:text-xl">
             From checkout to global sales tax compliance, companies around the world use Flowbite to simplify their payment stack.
           </p>
           <div className="mt-6 flex ">
-            <a href="#" className="inline-block mt-3 px-6 py-4 text-lg font-medium text-center text-gray-800 border rounded-lg border-gray-300 hover:bg-gray-100 focus:ring-4 focus:ring-gray-100 dark:text-white dark:border-gray-700 dark:hover:bg-gray-700 dark:focus:ring-gray-800">
+            <a href="#" className="inline-block mt-3 px-6 py-4 text-lg font-medium text-center text-gray-800 border rounded-lg border-gray-300 hover:bg-gray-100 focus:ring-4 focus:ring-gray-100 dark:text-white ">
               Get started
             </a>
-            <a href="#" className="mx-2 inline-block mt-3 px-6 py-4 text-lg font-medium text-center text-gray-800 border rounded-lg border-gray-300 hover:bg-gray-100 focus:ring-4 focus:ring-gray-100 dark:text-white dark:border-gray-700 dark:hover:bg-gray-700 dark:focus:ring-gray-800">
+            <a href="#" className="mx-2 inline-block mt-3 px-6 py-4 text-lg font-medium text-center text-gray-800 border rounded-lg border-gray-300 hover:bg-gray-100 focus:ring-4 focus:ring-gray-100 dark:text-white ">
               Speak to Sales
             </a>
           </div>
