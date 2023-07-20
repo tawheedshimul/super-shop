18266c1160aaad20592cd24144dee0dbdf54e1c3
tawheedshimul <tawheedshimul@gmail.com>
2023-07-20 15:48:51 +0600

    update

--- src/Footer/FixedFooter/FixedFooter.jsx
+++ src/Footer/FixedFooter/FixedFooter.jsx
@@ -2,30 +2,70 @@ import React from 'react';
 import { GiAllForOne } from 'react-icons/gi';
 
 const Footer = () => {
-  return (
-    <footer className="bg-gray-200 py-2">
-      <div className="container mx-auto flex justify-around">
-        <button className="flex items-center bg-blue-300 px-4 py-2 rounded-lg hover:bg-blue-400 focus:outline-none focus:ring focus:ring-blue-300">
-          {/* <RiShirtLine className="text-2xl mr-2" /> */}
-          <GiAllForOne></GiAllForOne>
-          <span>All</span>
-        </button>
-        <button className="flex items-center px-4 py-2 rounded-lg hover:bg-gray-300 focus:outline-none focus:ring focus:ring-gray-200">
-          {/* <RiMenLine className="text-2xl mr-2" /> */}
-          
-          <span>Mens</span>
-        </button>
-        <button className="flex items-center px-4 py-2 rounded-lg hover:bg-gray-300 focus:outline-none focus:ring focus:ring-gray-200">
-          {/* <RiWomenLine className="text-2xl mr-2" /> */}
-          <span>Womens</span>
-        </button>
-        <button className="flex items-center px-4 py-2 rounded-lg hover:bg-gray-300 focus:outline-none focus:ring focus:ring-gray-200">
-          {/* <RiChildLine className="text-2xl mr-2" /> */}
-          <span>Kids</span>
-        </button>
-      </div>
-    </footer>
-  );
+    return (
+        <footer className="bg-gray-200 py-2">
+            <div className="container mx-auto flex justify-around">
+                <button className="flex items-center bg-blue-300 px-4 py-2 rounded-lg hover:bg-blue-400 focus:outline-none focus:ring focus:ring-blue-300">
+                    {/* <RiShirtLine className="text-2xl mr-2" /> */}
+                    <GiAllForOne></GiAllForOne>
+                    <span>All</span>
+                </button>
+                <button className="flex items-center px-4 py-2 rounded-lg hover:bg-gray-300 focus:outline-none focus:ring focus:ring-gray-200">
+                    <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        width="24"
+                        height="24"
+                        viewBox="0 0 24 24"
+                        fill="none"
+                        stroke="currentColor"
+                        strokeWidth="2"
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                    >
+                        <circle cx="12" cy="4" r="2" />
+                        <path d="M5 20h14a2 2 0 0 0 2-2V8a2 2 0 0 0-2-2h-3.7L9.7 19.3a1 1 0 0 1-1.4 0L5 15.6V18a2 2 0 0 0 2 2z" />
+                    </svg>
+
+                    <span>Mens</span>
+                </button>
+                <button className="flex items-center px-4 py-2 rounded-lg hover:bg-gray-300 focus:outline-none focus:ring focus:ring-gray-200">
+                    <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        width="24"
+                        height="24"
+                        viewBox="0 0 24 24"
+                        fill="none"
+                        stroke="currentColor"
+                        strokeWidth="2"
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                    >
+                        <circle cx="12" cy="5" r="2" />
+                        <path d="M12 22s-6-3-6-8V6s3-2 6-2 6 2 6 2v8c0 5-6 8-6 8z" />
+                    </svg>
+                    <span>Womens</span>
+                </button>
+                <button className="flex items-center px-4 py-2 rounded-lg hover:bg-gray-300 focus:outline-none focus:ring focus:ring-gray-200">
+                    <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        width="24"
+                        height="24"
+                        viewBox="0 0 24 24"
+                        fill="none"
+                        stroke="currentColor"
+                        strokeWidth="2"
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                    >
+                        <circle cx="12" cy="4" r="2" />
+                        <path d="M12 2s-5 1.95-5 6v5s2 3 5 3 5-3 5-3V8s-5-1.95-5-6z" />
+                        <path d="M12 15a3 3 0 0 1 3-3h0a3 3 0 0 1 3 3" />
+                    </svg>
+                    <span>Kids</span>
+                </button>
+            </div>
+        </footer>
+    );
 };
 
 export default Footer;
