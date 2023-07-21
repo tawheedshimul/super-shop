c442ed585e083492a6a070ca5fb3c4a8c0b24e24
tawheedshimul <tawheedshimul@gmail.com>
2023-07-21 23:59:58 +0600

    update

--- src/Header/SearchBar/SearchBar.jsx
+++ src/Header/SearchBar/SearchBar.jsx
@@ -1,14 +1,17 @@
 import React from 'react'
-import { IoMdArrowDropdown, IoMdSearch } from 'react-icons/io';
+import './SearchBar.css'
+import { IoMdSearch } from 'react-icons/io';
 
 function SearchBar() {
     return (
         <div className="relative">
             <div className='relative flex h-10 w-full overflow-clip'>
-                <button className="flex items-center border border-slate-400 bg-slate-50 px-2 text-sm text-slate-400 transition-colors duration-300">
-                    <p className="mr-1">Products</p>
-                    <IoMdArrowDropdown className="text-xl" />
-                </button>
+                <select name="product-names" id="product-names" className="flex items-center border border-slate-400 bg-slate-50 px-2 text-sm text-slate-400 transition-colors duration-300">
+                    <option value="rigatoni">All</option>
+                    <option value="dave">Men</option>
+                    <option value="pumpernickel">Women</option>
+                    <option value="reeses">Kids</option>
+                </select>
                 <input
                     type="text"
                     placeholder="Search Products by Titles or Tag"
